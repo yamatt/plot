6c4afff153e13cf6304e2d440c31021664be96ca
Matt Copperwaite <matthew.copperwaite@ft.com>
2024-01-04 11:55:13 +0000

    handle youtube specifically

--- src/js/panel.ts
+++ src/js/panel.ts
@@ -1,10 +1,11 @@
 export default class Panel {
 
+    #youtube_video_id_regex = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/gi;
     #form: HTMLFormElement;
 
     get form() {
         if (!this.#form) {
-            this.#form =  document.getElementsByTagName("form")[0];
+            this.#form = document.getElementsByTagName("form")[0];
         }
         return this.#form;
     }
@@ -14,7 +15,16 @@ export default class Panel {
             e.preventDefault();
             const form = <HTMLFormElement> e.target;
             const data = new FormData(form);
-            document.location.href = <string> data.get('url');
+            const url = <string> data.get('url');
+
+            if(url.startsWith("https://www.youtube.com/watch?v=")) {
+                const video_id = url.match(this.#youtube_video_id_regex);
+                const video_url = `https://www.youtube.com/embed/${video_id}?enablejsapi=1`;
+                document.location.href = video_url;
+            }
+            else {
+                document.location.href = url;
+            }
         })
     }
 }
